f0d3ba8df16b12c65a6e3336af23b3708ac2a177
Prabhjot Singh <prabhjotlamba2001@gmail.com>
2020-10-04 02:16:34 +0530

    Added Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,63 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Footer.css'
+
+import logo from './Media/logo.png'
 
 function Footer() {
     return (
         <div className="footer">
-            
+
+            <div className="footer__logo">
+                <Link to="/" >
+                    <img className="logo" src={logo} alt="logo" />
+                </Link>
+            </div>
+
+            <div className="footer__contact">
+
+                <h3>Developed By</h3>
+
+                <div className="footer__contact__details">
+
+                    <div className="footer__contact__individual">
+
+                        <h5>
+                            Prabhjot Singh<br />
+                            prabhjotlamba2001@gmail.com<br />
+                            8882717172<br />
+                        </h5>
+
+                    </div>
+
+                    <div className="footer__contact__individual">
+                        <h5>
+                            Abhijeet Varun<br />
+                            abhijeetvarun@gmail.com<br />
+                            8851785099<br />
+                        </h5>
+                    </div>
+
+                    <div className="footer__contact__individual">
+
+                        <h5>
+                            Sumit Saurabh<br />
+                            sumitsaurabh9868@gmail.com<br />
+                            9871464772<br />
+                        </h5>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div className="footer__icons">
+
+            </div>
+
+
         </div>
     )
 }
