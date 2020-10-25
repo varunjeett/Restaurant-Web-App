361f54a0f7e35b6010b68edc7d76a439c66186d5
unknown <jayant.it19@nsut.ac.in>
2020-10-25 13:28:38 +0530

    Search Bar functionality

--- src/Booking.js
+++ src/Booking.js
@@ -1,184 +1,184 @@
-import React from "react";
-import { db } from "./firebase";
-import "./Booking.css";
-import * as Yup from "yup";
-import { Formik } from "formik";
-import Confirmation from "./Confirmation";
-import ReactDOMServer from "react-dom/server";
-import jsPDF from "jspdf";
+// import React from "react";
+// import { db } from "./firebase";
+// import "./Booking.css";
+// import * as Yup from "yup";
+// import { Formik } from "formik";
+// import Confirmation from "./Confirmation";
+// import ReactDOMServer from "react-dom/server";
+// import jsPDF from "jspdf";
 
-const Booking = () => (
-  <Formik
-    initialValues={{
-      name: "",
-      number: "",
-      email: "",
-      date: "",
-      time: "",
-      checked:false
-    }}
-    onSubmit={(values, { resetForm }) => {
+// const Booking = () => (
+//   <Formik
+//     initialValues={{
+//       name: "",
+//       number: "",
+//       email: "",
+//       date: "",
+//       time: "",
+//       checked:false
+//     }}
+//     onSubmit={(values, { resetForm }) => {
       
-      // if  checkbox is checked, AUTO DOWNLOAD the pdf of BOOKING RECEIPT
+//       // if  checkbox is checked, AUTO DOWNLOAD the pdf of BOOKING RECEIPT
       
 
-      let booking={
-          name: values.name,
-          number: values.number,
-          email: values.email,
-          time: values.time,
-          date: values.date,
-          timestamp: new Date().getTime(),
-      }
+//       let booking={
+//           name: values.name,
+//           number: values.number,
+//           email: values.email,
+//           time: values.time,
+//           date: values.date,
+//           timestamp: new Date().getTime(),
+//       }
 
-      alert(booking.name);
+//       alert(booking.name);
 
-      db.collection("Bookings")
-        .add(booking)
-        .then(alert("Booking Done, We'll Be Waiting For You !!!"))
-        .catch((error) => {
-          alert(error.message);
-        });
+//       db.collection("Bookings")
+//         .add(booking)
+//         .then(alert("Booking Done, We'll Be Waiting For You !!!"))
+//         .catch((error) => {
+//           alert(error.message);
+//         });
 
-      if(values.checked===true){
-        var doc = new jsPDF();
-        alert("hello");
-        doc.fromHTML(ReactDOMServer.renderToStaticMarkup(<Confirmation booking={booking}/>));
-        alert("world");
-        doc.save("myDocument.pdf");
-      }
+//       if(values.checked===true){
+//         var doc = new jsPDF();
+//         alert("hello");
+//         doc.fromHTML(ReactDOMServer.renderToStaticMarkup(<Confirmation booking={booking}/>));
+//         alert("world");
+//         doc.save("myDocument.pdf");
+//       }
 
-      resetForm({ values: "" });
-    }}
-    validationSchema={Yup.object().shape({
-      name: Yup.string().required("Name is required"),
-      email: Yup.string().email(),
-      number: Yup.string()
-        .required("Number is required")
-        .length(10, "Length should be equal to 10"),
-      date: Yup.string().required("Date is required"),
-      time: Yup.string().required("Arrival Time is required"),
-      checked: Yup.bool(),
-    })}
-  >
-    {(props) => {
-      const {
-        values,
-        touched,
-        errors,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        setFieldValue
-      } = props;
+//       resetForm({ values: "" });
+//     }}
+//     validationSchema={Yup.object().shape({
+//       name: Yup.string().required("Name is required"),
+//       email: Yup.string().email(),
+//       number: Yup.string()
+//         .required("Number is required")
+//         .length(10, "Length should be equal to 10"),
+//       date: Yup.string().required("Date is required"),
+//       time: Yup.string().required("Arrival Time is required"),
+//       checked: Yup.bool(),
+//     })}
+//   >
+//     {(props) => {
+//       const {
+//         values,
+//         touched,
+//         errors,
+//         handleChange,
+//         handleBlur,
+//         handleSubmit,
+//         setFieldValue
+//       } = props;
   
-      return (
-        <div className="booking">
-          <div className="booking__box">
-            <div className="booking__img">
-              <h1>Let us serve you better</h1>
-            </div>
-            <div className="booking__form">
-              <form onSubmit={handleSubmit} className="booking__table">
-                <div className="inputform__booking">
-                  <label htmlFor="name">Name:</label>
-                  <input
-                    name="name"
-                    type="text"
-                    value={values.name}
-                    placeholder=" Enter your Name"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  {errors.name && touched.name && (
-                    <div className="input-feedback-booking">*{errors.name}</div>
-                  )}
-                </div>
+//       return (
+//         <div className="booking">
+//           <div className="booking__box">
+//             <div className="booking__img">
+//               <h1>Let us serve you better</h1>
+//             </div>
+//             <div className="booking__form">
+//               <form onSubmit={handleSubmit} className="booking__table">
+//                 <div className="inputform__booking">
+//                   <label htmlFor="name">Name:</label>
+//                   <input
+//                     name="name"
+//                     type="text"
+//                     value={values.name}
+//                     placeholder=" Enter your Name"
+//                     onChange={handleChange}
+//                     onBlur={handleBlur}
+//                   />
+//                   {errors.name && touched.name && (
+//                     <div className="input-feedback-booking">*{errors.name}</div>
+//                   )}
+//                 </div>
 
-                <div className="inputform__booking">
-                  <label htmlFor="email">Email (Optional):</label>
-                  <input
-                    name="email"
-                    type="email"
-                    value={values.email}
-                    placeholder=" Enter your Email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  {errors.email && touched.email && (
-                    <div className="input-feedback-booking">*{errors.email}</div>
-                  )}
-                </div>
+//                 <div className="inputform__booking">
+//                   <label htmlFor="email">Email (Optional):</label>
+//                   <input
+//                     name="email"
+//                     type="email"
+//                     value={values.email}
+//                     placeholder=" Enter your Email"
+//                     onChange={handleChange}
+//                     onBlur={handleBlur}
+//                   />
+//                   {errors.email && touched.email && (
+//                     <div className="input-feedback-booking">*{errors.email}</div>
+//                   )}
+//                 </div>
 
-                <div className="inputform__booking">
-                  <label htmlFor="number">Contact No.:</label>
-                  <input
-                    name="number"
-                    type="tel"
-                    value={values.number}
-                    placeholder=" Enter your Contact No."
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  {errors.number && touched.number && (
-                    <div className="input-feedback-booking">*{errors.number}</div>
-                  )}
-                </div>
+//                 <div className="inputform__booking">
+//                   <label htmlFor="number">Contact No.:</label>
+//                   <input
+//                     name="number"
+//                     type="tel"
+//                     value={values.number}
+//                     placeholder=" Enter your Contact No."
+//                     onChange={handleChange}
+//                     onBlur={handleBlur}
+//                   />
+//                   {errors.number && touched.number && (
+//                     <div className="input-feedback-booking">*{errors.number}</div>
+//                   )}
+//                 </div>
 
-                <div className="inputform__booking">
-                  <label htmlFor="date">Booking Date:</label>
-                  <input
-                    name="date"
-                    type="text"
-                    value={values.date}
-                    placeholder=" Format : Date/Month"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  {errors.date && touched.date && (
-                    <div className="input-feedback-booking">*{errors.date}</div>
-                  )}
-                </div>
+//                 <div className="inputform__booking">
+//                   <label htmlFor="date">Booking Date:</label>
+//                   <input
+//                     name="date"
+//                     type="text"
+//                     value={values.date}
+//                     placeholder=" Format : Date/Month"
+//                     onChange={handleChange}
+//                     onBlur={handleBlur}
+//                   />
+//                   {errors.date && touched.date && (
+//                     <div className="input-feedback-booking">*{errors.date}</div>
+//                   )}
+//                 </div>
 
-                <div className="inputform__booking">
-                  <label htmlFor="time">Arrival Time:</label>
-                  <input
-                    name="time"
-                    type="text"
-                    value={values.time}
-                    placeholder=" Format : Hour:Min AM/PM"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  {errors.time && touched.time && (
-                    <div className="input-feedback-booking">*{errors.time}</div>
-                  )}
-                </div>
+//                 <div className="inputform__booking">
+//                   <label htmlFor="time">Arrival Time:</label>
+//                   <input
+//                     name="time"
+//                     type="text"
+//                     value={values.time}
+//                     placeholder=" Format : Hour:Min AM/PM"
+//                     onChange={handleChange}
+//                     onBlur={handleBlur}
+//                   />
+//                   {errors.time && touched.time && (
+//                     <div className="input-feedback-booking">*{errors.time}</div>
+//                   )}
+//                 </div>
 
 
-                <div className="inputform__booking">
-                  <label htmlFor="checked">Do you want soft copy of confimaration</label>
-                  <input
-                    name="checked"
-                    type="checkbox"
-                    value={values.checked}
-                    onClick={() => setFieldValue("checked", !values.checked)}
-                    onBlur={handleBlur}
-                  />
-                </div>
+//                 <div className="inputform__booking">
+//                   <label htmlFor="checked">Do you want soft copy of confimaration</label>
+//                   <input
+//                     name="checked"
+//                     type="checkbox"
+//                     value={values.checked}
+//                     onClick={() => setFieldValue("checked", !values.checked)}
+//                     onBlur={handleBlur}
+//                   />
+//                 </div>
 
-                <input
-                  className="booking__button"
-                  type="submit"
-                  onClick={handleSubmit}
-                />
-              </form>
-            </div>
-          </div>
-        </div>
-      );
-    }}
-  </Formik>
-);
+//                 <input
+//                   className="booking__button"
+//                   type="submit"
+//                   onClick={handleSubmit}
+//                 />
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       );
+//     }}
+//   </Formik>
+// );
 
-export default Booking;
+// export default Booking;
--- src/Confirmation.js
+++ src/Confirmation.js
@@ -1,33 +1,33 @@
-import React from "react";
-import "./confirmation.css";
-import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+// import React from "react";
+// import "./confirmation.css";
+// import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+// import logo from "./Media/logo.jpg";
 
-function Confirmation(props) {
+// function Confirmation(props) {
 
-  const user=props.booking;  
-  return (
-    <div>
-      <h1 id="heading">Your Booking has been confirmed</h1>
-      <div class="confirmation">
-        <div class="top">
-          <div class="logo">
-        // put company logo here
-            {/* <img src= /> */}
-          </div>
-          <div class="date">{user.date}</div>
-        </div>
-        <div class="bottom">
-          <p>Name : {user.name}</p>
-          <p>Contact Number : {user.number}</p>
-          <p>Email : {user.email}</p>
-          <p>Date : {user.date}</p>
-          <p>Time : {user.time}</p>
-          <p align="right">
-            <CheckCircleOutlineIcon fontSize="large" />
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-}
-export default Confirmation;
+//   const user=props.booking;  
+//   return (
+//     <div>
+//       <h1 id="heading">Your Booking has been confirmed</h1>
+//       <div class="confirmation">
+//         <div class="top">
+//           <div class="Logo">
+//             <img src={logo} alt=""/>
+//           </div>
+//           <div class="date">{user.date}</div>
+//         </div>
+//         <div class="bottom">
+//           <p>Name : {user.name}</p>
+//           <p>Contact Number : {user.number}</p>
+//           <p>Email : {user.email}</p>
+//           <p>Date : {user.date}</p>
+//           <p>Time : {user.time}</p>
+//           <p align="right">
+//             <CheckCircleOutlineIcon fontSize="large" />
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Confirmation;
