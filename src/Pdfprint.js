50a646b69e2d3f78ad6fc0c0fbfc980e3a409f93
Sumit <sumitsaurabh9868@gmail.com>
2021-06-29 23:00:25 +0530

    edited print pdf

--- src/Pdfprint.js
+++ src/Pdfprint.js
@@ -10,24 +10,25 @@ function Pdfprint() {
   const [{ id, name, contact, arrivalTime, bookingDate }] = useStateValue();
   return (
     <div className="Pdfprint">
+      <div className="printMessage">
       <h1>Hello User, Your Booking is Confirmed</h1>
       <h1>Download your Receipt by clicking on the Button below</h1>
+      </div>
 
       <div className="receipt" ref={ref}>
         <img src={logo} alt="logo of BOL" />
-        <h1>{id}</h1>
-
-        <h1>{name}</h1>
-
-        <h1>{contact}</h1>
-
-        <h1>{arrivalTime}</h1>
-
-        <h1>{bookingDate}</h1>
+        <div className="pdftext">
+          <p>{id}</p>
+          <p>{name}</p>
+          <p>{contact}</p>
+          <p>{arrivalTime}</p>
+          <p>{bookingDate}</p>
+        </div>
+        
       </div>
 
       <Pdf targetRef={ref} filename="booking.pdf">
-        {({ toPdf }) => <button onClick={toPdf}>Download your receipt</button>}
+        {({ toPdf }) => <button className="pdf__button" onClick={toPdf}>Download your receipt</button>}
       </Pdf>
     </div>
   );
--- src/Scrolling.js
+++ src/Scrolling.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Scrolling.css";
+import scrollFirst from './Media/mainPage.jpg'
 
 function Scrolling() {
   return (
     <div className="scrolling">
       
-      <div className="scrolling__firstPage"></div>
+      <div className="scrolling__firstPage">
+        <div className="scrollFirstImage">
+        {/* <img src={scrollFirst} alt="main page image" /> */}
+        </div>
+      </div>
 
       <div className="scrolling__secondPage">
         <h2>
